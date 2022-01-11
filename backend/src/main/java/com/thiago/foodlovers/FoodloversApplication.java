956bb8f951c7fd0021da71b561b06879f4ec9b7c
Thiago Martins <devthiagomartins@gmail.com>
2022-01-11 09:53:10 -0300

    Project created

---
+++ backend/src/main/java/com/thiago/foodlovers/FoodloversApplication.java
@@ -0,0 +1,13 @@
+package com.thiago.foodlovers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodloversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodloversApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/thiago/foodlovers/FoodloversApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiago.foodlovers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodloversApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
